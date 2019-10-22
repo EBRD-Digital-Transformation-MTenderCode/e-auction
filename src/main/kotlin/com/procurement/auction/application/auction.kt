a158a0d55c26f1dd4ba3971d66ce6531d82ccd5e
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-10-22 16:52:33 +0300

    ES-1938 Added the function for calculating max count auctions.

--- src/main/kotlin/com/procurement/auction/application/auction.kt
+++ src/main/kotlin/com/procurement/auction/application/auction.kt
@@ -1,6 +1,7 @@
 package com.procurement.auction.application
 
 import java.time.Duration
+import java.time.LocalTime
 
 fun auctionDuration(
     durationOneStep: Duration,
@@ -14,3 +15,21 @@ fun auctionDuration(
     val durationAllRounds = durationOneRound.multipliedBy(qtyRounds)
     return durationAllRounds + durationPauseAfterAuction
 }
+
+fun countAuctions(
+    auctionDuration: Duration,
+    slots: List<Pair<LocalTime, LocalTime>>
+): Long {
+    return slots.asSequence()
+        .map { slot ->
+            countAuctionsBySlot(auctionDuration = auctionDuration, start = slot.first, end = slot.second)
+        }
+        .sum()
+}
+
+private fun countAuctionsBySlot(auctionDuration: Duration, start: LocalTime, end: LocalTime): Long {
+    val slotDuration: Duration = slotDuration(start, end)
+    return slotDuration.seconds / auctionDuration.seconds
+}
+
+private fun slotDuration(start: LocalTime, end: LocalTime) = Duration.between(start, end)
--- src/test/kotlin/com/procurement/auction/application/AuctionTest.kt
+++ src/test/kotlin/com/procurement/auction/application/AuctionTest.kt
@@ -1,10 +1,15 @@
 package com.procurement.auction.application
 
+import com.procurement.auction.domain.model.date.JsonTimeDeserializer
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.time.Duration
+import java.time.LocalTime
 
 class AuctionTest {
+    companion object {
+        private val AUCTION_DURATION = Duration.ofSeconds(2550)
+    }
 
     @Test
     fun auctionDurationTest() {
@@ -16,6 +21,27 @@ class AuctionTest {
             durationPauseAfterAuction = Duration.ofSeconds(30)
         )
 
-        assertEquals(Duration.ofSeconds(2550), actualAuctionDuration)
+        assertEquals(AUCTION_DURATION, actualAuctionDuration)
     }
+
+    @Test
+    fun countAuctionsTest() {
+        val slots = listOf(
+            "06:00Z".toLocalTime() to "13:00Z".toLocalTime(),
+            "07:00Z".toLocalTime() to "13:00Z".toLocalTime(),
+            "08:00Z".toLocalTime() to "13:00Z".toLocalTime(),
+            "09:00Z".toLocalTime() to "13:00Z".toLocalTime(),
+            "10:00Z".toLocalTime() to "13:00Z".toLocalTime(),
+            "11:00Z".toLocalTime() to "13:00Z".toLocalTime(),
+            "12:00Z".toLocalTime() to "13:00Z".toLocalTime()
+        )
+        val actualAuctionDuration = countAuctions(
+            auctionDuration = AUCTION_DURATION,
+            slots = slots
+        )
+
+        assertEquals(36, actualAuctionDuration)
+    }
+
+    private fun String.toLocalTime(): LocalTime = JsonTimeDeserializer.deserialize(this)
 }
