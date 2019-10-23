d5edb180c9d5e4276e828d403543fbd74a5ed8b4
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-10-23 12:03:07 +0300

    ES-1938 Added utility functions to check for uniqueness and transform collection to the set.

---
+++ src/main/kotlin/com/procurement/auction/application/collections.kt
@@ -0,0 +1,19 @@
+package com.procurement.auction.application
+
+inline fun <T, V> Collection<T>.isUnique(selector: (T) -> V): Boolean {
+    val unique = HashSet<V>()
+    forEach { item ->
+        if (!unique.add(selector(item))) return false
+    }
+    return true
+}
+
+inline fun <T, V> Collection<T>.isNotUnique(selector: (T) -> V): Boolean = !this.isUnique(selector)
+
+inline fun <T, V> Collection<T>.toSetBy(selector: (T) -> V): Set<V> {
+    val collections = LinkedHashSet<V>()
+    forEach {
+        collections.add(selector(it))
+    }
+    return collections
+}
