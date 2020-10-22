3f19a553250f3f4bd9bebb9a06540b75bfb96bf6
katherine <kate.shaidurova@gmail.com>
2020-10-22 14:18:59 +0300

    ES-4480 Added validation errors

--- src/main/kotlin/com/procurement/auction/domain/fail/error/ValidationError.kt
+++ src/main/kotlin/com/procurement/auction/domain/fail/error/ValidationError.kt
@@ -8,4 +8,24 @@ sealed class ValidationError(
     val entityId: String? = null
 ) : Fail.Error("VR.COM-") {
     override val code: String = prefix + numberError
+
+    class DuplicateElectronicAuctionIds(duplicateId: String) : ValidationError(
+        numberError = "18.1.1",
+        description = "Found duplicate electronicAuctions.details.id '$duplicateId'"
+    )
+
+    class LinkedLotNotFound(lotIds: Set<String>) : ValidationError(
+        numberError = "18.1.2",
+        description = "Missing lots: '${lotIds.joinToString()}'"
+    )
+
+    class LotMustBeLinkedToOneAuction(lotId: String): ValidationError(
+        numberError = "18.1.3",
+        description = "Lot '$lotId' must be linked to one and only one electronic auction"
+    )
+
+    class AuctionModalityContainsInvalidCurrency(auctionIds: List<String>, tenderCurrency: String): ValidationError(
+        numberError = "18.1.4",
+        description = "Electronic auctions '${auctionIds.joinToString()}' contain modality(s) with currency that does not match tender currency '$tenderCurrency'"
+    )
 }
