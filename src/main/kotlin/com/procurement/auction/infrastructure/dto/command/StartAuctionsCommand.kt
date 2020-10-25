e873cc08aec273fd32546b4e9b22fe3adb89bf8c
Evgen Kopach <zkopach@gmail.com>
2020-10-25 10:55:48 +0200

    ES-4599 Added optional tender.value attribite in request at 'startAuction' command.

--- src/main/kotlin/com/procurement/auction/infrastructure/dto/command/StartAuctionsCommand.kt
+++ src/main/kotlin/com/procurement/auction/infrastructure/dto/command/StartAuctionsCommand.kt
@@ -1,6 +1,7 @@
 package com.procurement.auction.infrastructure.dto.command
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
@@ -93,8 +94,22 @@ data class StartAuctionsCommand(
 
             @field:JsonProperty("title") @param:JsonProperty("title") val title: String,
             @field:JsonProperty("description") @param:JsonProperty("description") val description: String,
-            @field:JsonProperty("lots") @param:JsonProperty("lots") val lots: List<Lot>
+            @field:JsonProperty("lots") @param:JsonProperty("lots") val lots: List<Lot>,
+
+            @JsonInclude(JsonInclude.Include.NON_NULL)
+            @field:JsonProperty("value") @param:JsonProperty("value") val value: Value?
         ) {
+
+
+            data class Value(
+                @JsonDeserialize(using = AmountDeserializer::class)
+                @JsonSerialize(using = AmountSerializer::class)
+                @field:JsonProperty("amount") @param:JsonProperty("amount") val amount: Amount,
+
+                @JsonDeserialize(using = CurrencyDeserializer::class)
+                @JsonSerialize(using = CurrencySerializer::class)
+                @field:JsonProperty("currency") @param:JsonProperty("currency") val currency: Currency
+            )
 
             data class Lot(
                 @JsonDeserialize(using = LotIdDeserializer::class)
